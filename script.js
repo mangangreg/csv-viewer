92145f6e6dedc927904f7e4e1526c15f190ed7a1
Greg Mangan <mangangreg@gmail.com>
2020-05-28 15:07:33 -0400

    Working upload

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 console.log('tools.js run')
 const csv = require("csv-parser")
 const fs = require('fs')
-const results = []
+const {dialog} = require('electron').remote
 
-let tableData = {
-	headers: [],
-	rows: []
-}
-fs.createReadStream('data/test1.csv')
-  .pipe(csv())
-  .on('data', (data) => tableData.rows.push(data))
+function updateTable(filepath){
+	let tableData = {
+		headers: [],
+		rows: []
+	}
+	let docTitle = filepath.split('\\').slice(-1)[0]
+	document.querySelector('#docTitle').innerHTML = docTitle
+
+	fs.createReadStream(filepath)
+	.pipe(csv())
+	.on('data', (data) => tableData.rows.push(data))
 	.on('headers', (headers) => tableData.headers = headers)
-  .on('end', () => {
-    console.log(tableData)
+	.on('end', () => {
+		console.log(tableData)
 		var table = Handlebars.compile(document.querySelector('#table-template').innerHTML)
 		document.querySelector('#table-wrapper').innerHTML = table(
 			{
@@ -20,8 +24,12 @@ fs.createReadStream('data/test1.csv')
 				rows:tableData.rows.map( obj => Object.values(obj) )
 			}
 		)
-  });
+	});
+}
 
-// document.addEventListener('DOMContentLoaded', function(){
-//     console.log('document ready')
-// }, false);
+function clickOpen(){
+	dialog.showOpenDialog({ properties: [ 'openFile' ]}).then((data) => {
+		console.log(data)
+		updateTable(data.filePaths[0])
+	})
+}
